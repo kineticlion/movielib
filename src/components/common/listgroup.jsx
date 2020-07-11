b2fa286a78d9b614cb27b38df5afa61c1aae24f0
Sufiyan <sufiyansaboowala13@gmail.com>
2020-07-11 16:47:34 -0400

    Added Listgroup Component

---
+++ src/components/common/listgroup.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ListGroup = (props) => {
+  const {
+    items,
+    valueProperty: value,
+    textProperty: text,
+    onItemSelect,
+    selectedItem,
+  } = props;
+
+  const listStyle = 'btn btn-sm list-group-item';
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <button
+          onClick={() => onItemSelect(item)}
+          key={item[value]}
+          className={selectedItem === item ? `${listStyle} active` : listStyle}
+          style={{ cursor: 'pointer' }}
+        >
+          {item[text]}
+        </button>
+      ))}
+    </ul>
+  );
+};
+
+ListGroup.defaultProps = {
+  textProperty: 'name',
+  valueProperty: '_id',
+};
+
+export default ListGroup;
